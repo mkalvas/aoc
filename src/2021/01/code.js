79b3cffa862b23a46b93d4db0fe23b88ab05f553
Mike Kalvas <michael.kalvas@beam.dental>
2021-12-01 17:45:11 -0500

    feat: 2021 day 01 updated answer

--- src/2021/01/code.js
+++ src/2021/01/code.js
@@ -1,22 +1,9 @@
-import { parseInts, sum } from '../../lib';
+import { cons, parseInts, sum } from '../../lib';
 
-const increases = (nums) => {
-  let increases = 0;
-  for (let i = 1; i < nums.length; i++) {
-    if (nums[i] > nums[i - 1]) increases++;
-  }
-  return increases;
-};
+const inc = (input) =>
+  cons(parseInts(input))
+    .map(([a, b]) => b > a)
+    .filter(Boolean).length;
 
-const threeWindowIncreases = (nums) => {
-  let increases = 0;
-  for (let i = 3; i < nums.length; i++) {
-    const prev = sum([nums[i - 1], nums[i - 2], nums[i - 3]]);
-    const curr = sum([nums[i], nums[i - 1], nums[i - 2]]);
-    if (curr > prev) increases++;
-  }
-  return increases;
-};
-
-export const solutionOne = (input) => increases(parseInts(input));
-export const solutionTwo = (input) => threeWindowIncreases(parseInts(input));
+export const solutionOne = (input) => inc(input);
+export const solutionTwo = (input) => inc(cons(parseInts(input), 3).map(sum));
--- src/lib/array.js
+++ src/lib/array.js
@@ -9,13 +9,23 @@ export const arrayEquals = (a, b) =>
   a.length === b.length &&
   a.every((v, i) => v === b[i]);
 
+export const takeEvery = (a, every = 2, offset = 0) =>
+  a.filter((_, i) => (i + offset) % every === 0);
+
+export const groupEvery = (a, every) => {
+  const groups = [];
+  for (let i = 0; i < a.length; i += every) {
+    groups.push(a.slice(i, i + every));
+  }
+  return groups;
+};
+
 export const zip = (a, b) =>
   a.length >= b.length
     ? a.map((v, i) => [v, b[i]])
     : b.map((v, i) => [a[i], v]);
 
-export const zipSelf = (a) =>
-  a.reduce((z, v, i) => (i < 1 ? [] : [...z, [a[i - 1], v]]), []);
+export const cons = (a, size = 2) =>
+  Array.from({ length: a.length - size + 1 }, (_, i) => a.slice(i, i + size));
 
-export const takeEvery = (a, every = 2, offset = 0) =>
-  a.filter((_, i) => (i + offset) % every === 0);
+export const diffSelf = (a) => zipSelf(a).map(([a, b]) => b - a);
