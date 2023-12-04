b33dd385d77f2c626edf78d36c8f036d921e1475
Mike Kalvas <mkalvas@samcart.com>
2023-12-04 13:25:36 -0500

    feat: 2023 day 04

---
+++ src/2023/04/code.js
@@ -0,0 +1,27 @@
+import '../../lib/extensions';
+import { aofl } from '../../lib';
+
+const ws = /\s+/g;
+
+const score = (input) =>
+  input
+    .map((l) => l.split(': ')[1].split(' | '))
+    .map((card) => {
+      const [winning, have] = card.map((s) => s.trim().split(ws));
+      const count = have.filter((m) => winning.includes(m)).length;
+      return { score: count > 0 ? 2 ** (count - 1) : 0, count: count };
+    });
+
+export const solutionOne = (input) =>
+  score(input).reduce((s, { score }) => s + score, 0);
+
+export const solutionTwo = (input) => {
+  const scores = score(input).map((e) => e.count);
+  let copies = aofl(input.length, 1);
+  for (const [i, score] of scores.entries()) {
+    for (let j = 1; j < Math.min(input.length, score + 1); j++) {
+      copies[i + j] += copies[i];
+    }
+  }
+  return copies.sum();
+};
---
+++ src/2023/04/index.js
@@ -0,0 +1,11 @@
+import { solutionOne, solutionTwo } from './code';
+
+const day = {
+  label: 'Card scores',
+  path: `${__dirname}/input.txt`,
+  solutionOne,
+  solutionTwo,
+  answers: [24175, 18846301],
+};
+
+export default day;
